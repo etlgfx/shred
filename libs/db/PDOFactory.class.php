158e57bdc08b2b6e286ad2da7ba9717b48c84f04
Eric Liang <etlgfx@gmail.com>
2012-11-13 16:44:24 -0500

    pdo connection mode exception

--- libs/db/PDOFactory.class.php
+++ libs/db/PDOFactory.class.php
@@ -20,6 +20,7 @@ class PDOFactory {
 				isset($dbconfig['password']) ? $dbconfig['password'] : null,
 				array(
 					PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 				)
 			);
 		}
