1cad50eb7963332ebe605d253fb67bfef641968b
Eric Liang <etlgfx@gmail.com>
2013-02-15 17:59:10 -0500

    make pdo factory respect empty db passwords

--- libs/db/PDOFactory.class.php
+++ libs/db/PDOFactory.class.php
@@ -17,7 +17,7 @@ class PDOFactory {
 			$dbs[$db_name] = $return = new PDO(
 				"{$dbconfig['driver']}:host={$dbconfig['server']};dbname={$dbconfig['database']}",
 				isset($dbconfig['username']) ? $dbconfig['username'] : null,
-				isset($dbconfig['password']) ? $dbconfig['password'] : null,
+				empty($dbconfig['password']) ? null : $dbconfig['password'],
 				array(
 					PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
